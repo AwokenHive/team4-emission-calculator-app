6d8f2685d3772c0080601f41a0770997a167b160
Moeez Abdul <moeezrajput2000@gmail.com>
2024-02-27 11:47:47 +0000

    GetUnit (Miles or Km) function created

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -2,14 +2,21 @@ import React, { useRef } from "react";
 
 function Form() {
   const distanceRef = useRef(null);
-  const selectRef = useRef(null);
+  const mileageRef = useRef(null);
+  const vehicleRef = useRef(null);
   const hybridRef = useRef(null);
 
   function handleSubmit(event) {
     event.preventDefault();
+    const unit = getUnit();
     console.log("Distance value:", distanceRef.current.value);
-    console.log("Select value:", selectRef.current.value);
+    console.log("Mileage unit:", unit);
+    console.log("Vehicle type:", vehicleRef.current.value);
     console.log("Hybrid value:", hybridRef.current.checked);
+  }
+
+  const getUnit = () => {
+    return mileageRef.current.value;
   }
 
   return (
@@ -22,14 +29,14 @@ function Form() {
         </label>
         <label>
           <p>Mileage:</p>
-          <select ref={selectRef}>
+          <select ref={mileageRef}>
             <option value="miles">miles</option>
             <option value="km">km</option>
           </select>
         </label>
         <label>
           <p>Choose Vehicle:</p>
-          <select ref={selectRef}>
+          <select ref={vehicleRef}>
             <option value="avCarType">Average Car, Van and Motorbike</option>
           </select>
         </label>
