5ac741acff5b1eb5e1573eaae772dec3c65a3700
Ben Welland <wellandbb@gmail.com>
2024-02-27 12:17:32 +0000

    electric car animation

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import "./navbar.css";
 import { IoIosLeaf } from "react-icons/io";
+import lottie from "lottie-web";
+import { defineElement } from "@lordicon/element";
 
 const leafStyle = {color: "green"}
+const carStyle = {width:'250px',height:'250px'}
+
+
+// define "lord-icon" custom element with default properties
+defineElement(lottie.loadAnimation);
 
 const Navbar = () => {
   return (
     <div className="logo">
-      <h1>E4 Emissions<IoIosLeaf style={leafStyle}/></h1>
+      <h1>
+        E4 Emissions
+        <IoIosLeaf style={leafStyle}/>
+      </h1>
+      <lord-icon trigger="hover" src="https://cdn.lordicon.com/grcrdebf.json" colors="primary:#ffffff,secondary:#08a88a" style={carStyle}></lord-icon>
+      
     </div>
   );
 };
